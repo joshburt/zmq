190d552f8d3b36813415f0526ce09c5bc1e9bb99
Joshua C. Burt <joshburt@shapeandshare.com>
2016-04-17 09:11:09 -0600

    typo

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'zmq'
 maintainer       'Joshua C. Burt'
 maintainer_email 'joshburt@shapeandshare.com'
 license          'Apache License'
-description      'Installs/Configures zermq'
+description      'Installs/Configures zeromq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 issues_url 'https://github.com/joshburt/zmq/issues'
 source_url 'https://github.com/joshburt/zmq'
